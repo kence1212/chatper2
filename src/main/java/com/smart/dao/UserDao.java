5ac0ebf3b9178326c5301acc43b360e87a469b5b
kence <568034555@qq.com>
2018-03-31 17:09:02 +0800

    增加文件内容2

--- src/main/java/com/smart/dao/UserDao.java
+++ src/main/java/com/smart/dao/UserDao.java
@@ -1,7 +1,11 @@
 package com.smart.dao;
 
+import org.springframework.stereotype.Repository;
+
 /**
  * Created by 李泽龙 on 2018/3/31.
  */
+@Repository
 public class UserDao {
+
 }
--- src/main/java/com/smart/domain/Loginlog.java
+++ src/main/java/com/smart/domain/Loginlog.java
@@ -1,7 +1,45 @@
 package com.smart.domain;
 
+import java.util.Date;
+
 /**
  * Created by 李泽龙 on 2018/3/31.
  */
 public class Loginlog {
+    private int loginLogId;
+    private int userId;
+    private String ip;
+    private Date loginDate;
+
+    public int getLoginLogId() {
+        return loginLogId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public Date getLoginDate() {
+        return loginDate;
+    }
+
+    public void setLoginLogId(int loginLogId) {
+        this.loginLogId = loginLogId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public void setLoginDate(Date loginDate) {
+        this.loginDate = loginDate;
+    }
 }
