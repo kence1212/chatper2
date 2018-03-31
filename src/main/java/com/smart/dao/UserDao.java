a2c0c5198cbdabea0ba5fee7d8bd9bbc3ac5ae78
kence <568034555@qq.com>
2018-03-31 17:07:16 +0800

    增加文件内容

---
+++ src/main/java/com/smart/dao/UserDao.java
@@ -0,0 +1,7 @@
+package com.smart.dao;
+
+/**
+ * Created by 李泽龙 on 2018/3/31.
+ */
+public class UserDao {
+}
---
+++ src/main/java/com/smart/domain/Loginlog.java
@@ -0,0 +1,7 @@
+package com.smart.domain;
+
+/**
+ * Created by 李泽龙 on 2018/3/31.
+ */
+public class Loginlog {
+}
