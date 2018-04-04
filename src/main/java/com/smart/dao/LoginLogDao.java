96977f7ba84ff426e6324050d09c7f85318323a8
kence <568034555@qq.com>
2018-04-04 16:41:18 +0800

    LoginLog数据交互层源码编写

--- src/main/java/com/smart/dao/LoginLogDao.java
+++ src/main/java/com/smart/dao/LoginLogDao.java
@@ -1,7 +1,15 @@
 package com.smart.dao;
 
+import com.smart.domain.Loginlog;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class LoginLogDao {
-    private JdbcTemplate j;
+    private JdbcTemplate jdbcTemplate;
+
+    //保存登录日志
+    private final static String INSERT_LOG_SQL = "INSERT INTO t_login_log(user_id,ip,login_datetime)" +
+            "VALUES(?,?,?)";
+    public void insertLoginLog(Loginlog loginLog){
+
+    }
 }
