6cec7708ee076b1ef7dca6efceb45616978a555c
kence <568034555@qq.com>
2018-04-09 10:31:52 +0800

    完成业务层和相关junit

--- src/main/java/com/smart/service/UserService.java
+++ src/main/java/com/smart/service/UserService.java
@@ -2,9 +2,11 @@ package com.smart.service;
 
 import com.smart.dao.LoginLogDao;
 import com.smart.dao.UserDao;
+import com.smart.domain.Loginlog;
 import com.smart.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
@@ -27,6 +29,17 @@ public class UserService {
     }
 
     public User findUserByUserName(String userName){
-        
+        return userDao.findUserByUserName(userName);
+    }
+
+    @Transactional
+    public void loginSuccess(User user){
+        user.setCredits(5+user.getCredits());
+        Loginlog loginlog = new Loginlog();
+        loginlog.setUserId(user.getUserId());
+        loginlog.setIp(user.getLastIp());
+        loginlog.setLoginDate(user.getLastVisit());
+        userDao.updateLoginInfo(user);
+        loginLogDao.insertLoginLog(loginlog);
     }
 }
---
+++ src/test/java/com/smart/service/UserServiceTest.java
@@ -0,0 +1,32 @@
+package com.smart.service;
+
+import com.smart.domain.User;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
+import static org.testng.Assert.*;
+
+@ContextConfiguration("classpath*:smart-context.xml")
+public class UserServiceTest extends AbstractTransactionalTestNGSpringContextTests{
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Test
+    public void hasMatchUser(){
+        boolean b1 = userService.hasMatchUser("admin","123456");
+        boolean b2 = userService.hasMatchUser("admin","1111");
+        assertTrue(b1);
+        assertTrue(!b2);
+    }
+
+    @Test
+    public void findUserbyUserName(){
+        User user = userService.findUserByUserName("admin");
+        assertEquals(user.getUsername(),"admin");
+    }
+}
