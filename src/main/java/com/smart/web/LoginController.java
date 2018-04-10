b5d4852ef70872bb4c0f54295371ce9051f5c439
kence <568034555@qq.com>
2018-04-10 15:16:27 +0800

    添加展示层源码

---
+++ src/main/java/com/smart/web/LoginController.java
@@ -0,0 +1,21 @@
+package com.smart.web;
+
+import com.smart.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping(value = "/index.html")
+    public String loginPage(){
+        return "login";
+    }
+}
